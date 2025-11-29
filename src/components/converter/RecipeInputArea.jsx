3a3ff05547ac7faa7b2da4704905845fb9ea19b3
andrewliamc <77427633+andrewliamc@users.noreply.github.com>
2025-11-29 02:48:38 -0500

    Update Recipt Input

--- src/components/converter/RecipeInputArea.jsx
+++ src/components/converter/RecipeInputArea.jsx
@@ -2,13 +2,21 @@ import TextRecipeInput from './TextRecipeInput'
 import ImageRecipeUpload from './ImageRecipeUpload'
 import PdfRecipeUpload from './PdfRecipeUpload'
 
-export default function RecipeInputArea({ mode, textValue, onTextChange, onFileChange }) {
+export default function RecipeInputArea({
+  mode,
+  textValue,
+  onTextChange,
+  onFileChange,
+  ocrStatus,
+  ocrError,
+  isOcrRunning,
+}) {
   if (mode === 'image') {
-    return <ImageRecipeUpload onFileChange={onFileChange} />
+    return <ImageRecipeUpload onFileChange={onFileChange} status={ocrStatus} error={ocrError} isLoading={isOcrRunning} />
   }
 
   if (mode === 'pdf') {
-    return <PdfRecipeUpload onFileChange={onFileChange} />
+    return <PdfRecipeUpload onFileChange={onFileChange} status={ocrStatus} error={ocrError} isLoading={isOcrRunning} />
   }
 
   return <TextRecipeInput value={textValue} onChange={onTextChange} />
